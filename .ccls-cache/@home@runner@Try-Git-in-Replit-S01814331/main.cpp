59420d3699635372362a0add8ec1315b3fc9f53d
ZakBegley <144624673+ZakBegley@users.noreply.github.com>
2023-09-10 20:26:01 +0000

    Initial HelloWorld2 commit

--- .ccls-cache/@home@runner@Try-Git-in-Replit-S01814331/main.cpp
+++ .ccls-cache/@home@runner@Try-Git-in-Replit-S01814331/main.cpp
@@ -1,5 +1,13 @@
+
 #include <iostream>
 
-int main() {
+int main() 
+{
+  std::string YourName;//Define a string YourName.
+  std::cout << "What is your name?";//Prompt user to input YourName variable.
+  std::cin >> YourName;//Take user input
+  std::string greeting = "Hello world " + YourName + "!";//Define string variable "greeting" as the Hello World message + user input.
+  std::cout << greeting;//Output customized greeting for user to console.
+  
   return 0;
 }
